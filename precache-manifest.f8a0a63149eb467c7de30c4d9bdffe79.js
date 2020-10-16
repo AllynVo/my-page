04a16a3da6893bab37c745dee08f278cb832ef00
Allyn <allynvo@yahoo.com>
2020-10-16 15:19:42 -0700

    Updates

---
+++ precache-manifest.f8a0a63149eb467c7de30c4d9bdffe79.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcad14fe6d30a69b06a0a27d35a09e96",
+    "url": "/my-page/index.html"
+  },
+  {
+    "revision": "9b5acf32f02b0ade3c91",
+    "url": "/my-page/static/css/main.7b400fa8.chunk.css"
+  },
+  {
+    "revision": "f2e0ad99edc0101087f7",
+    "url": "/my-page/static/js/2.5673b3a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-page/static/js/2.5673b3a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5acf32f02b0ade3c91",
+    "url": "/my-page/static/js/main.09da7191.chunk.js"
+  },
+  {
+    "revision": "8c5b0f1d7ef53eefa48d",
+    "url": "/my-page/static/js/runtime-main.0de95849.js"
+  },
+  {
+    "revision": "4e54cf7595d8454f30cdafec1cc74ef9",
+    "url": "/my-page/static/media/Allyn_Resume_10-1.4e54cf75.pdf"
+  },
+  {
+    "revision": "c4d3f5d2c3909ef5d247809614c64dc3",
+    "url": "/my-page/static/media/building.c4d3f5d2.svg"
+  },
+  {
+    "revision": "54208a403a47454033c913011c691b1b",
+    "url": "/my-page/static/media/code-inspect.54208a40.svg"
+  },
+  {
+    "revision": "325d8177065166b1c105a03078e7d43a",
+    "url": "/my-page/static/media/designer.325d8177.svg"
+  },
+  {
+    "revision": "bfa148ffd7bffd08cdfdd2ab39802af8",
+    "url": "/my-page/static/media/preferences.bfa148ff.svg"
+  },
+  {
+    "revision": "5cfd1d69b379c6f8e1e82b6bec0bf2d3",
+    "url": "/my-page/static/media/scrum-board.5cfd1d69.svg"
+  },
+  {
+    "revision": "7854866d14263d95e146a79dca58093e",
+    "url": "/my-page/static/media/sea-video.7854866d.mp4"
+  },
+  {
+    "revision": "2a1be3c8436d652b78da8041866d06d1",
+    "url": "/my-page/static/media/selfie-1.2a1be3c8.JPG"
+  }
+]);
